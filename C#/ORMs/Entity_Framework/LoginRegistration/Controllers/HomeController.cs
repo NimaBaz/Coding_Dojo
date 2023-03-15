6079191a9c3d35f2749f1c8e5cdf9f461b6714df
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-03-15 15:47:58 -0700

    Login and Reg asssisnment

---
+++ C#/ORMs/Entity_Framework/LoginRegistration/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿// Using statements
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LoginRegistration.Models;
+
+namespace LoginRegistration.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    // Add a private variable of type MyContext (or whatever you named your context file)
+    private MyContext _context;
+
+    // Here we can "inject" our context service into the constructor 
+    // The "logger" was something that was already in our code, we're just adding around it
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        // When our HomeController is instantiated, it will fill in _context with context
+        // Remember that when context is initialized, it brings in everything we need from DbContext
+        // which comes from Entity Framework Core
+        _context = context;
+    }
+
+
+//////////////////////////////////////////////////////!LOGIN/REG DASHBOARD//////////////////////////////////////////////////////////////////////////
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+//////////////////////////////////////////////////////!LOGIN/REG DASHBOARD//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////!DASHBOARD//////////////////////////////////////////////////////////////////////////
+
+    [SessionCheck]
+    [HttpGet("/dishes/show")]
+    public IActionResult ShowAllDishes()
+    {
+        // Now any time we want to access our database we use _context
+        List<Dish> AllDishes = _context.Dishes.ToList();
+        ViewBag.AllDishes = AllDishes;
+
+        return View("ShowAllDishes");
+    }
+
+//////////////////////////////////////////////////////!DASHBOARD//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////!404 PATH//////////////////////////////////////////////////////////////////////////
+
+    [HttpGet("{**path}")]
+    public IActionResult Unknown() {
+        System.Console.WriteLine("It is working!!");
+        return View("404");
+    }
+
+//////////////////////////////////////////////////////!404 PATH//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////!ERRORS//////////////////////////////////////////////////////////////////////////
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+//////////////////////////////////////////////////////!ERRORS//////////////////////////////////////////////////////////////////////////
+
+
+}
---
+++ C#/ORMs/Entity_Framework/LoginRegistration/Models/LoginUser.cs
@@ -0,0 +1,24 @@
+#pragma warning disable CS8618
+
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegistration.Models;
+
+public class LoginUser
+{
+    // No other fields!
+    [DataType(DataType.EmailAddress)]
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Invalid Email Address")]
+    public string EmailLogin { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    [MinLength(8, ErrorMessage = "Password must be 8 characters long")]
+    [PasswordValidation]
+    public string PasswordLogin { get; set; }
+
+}
+
