76fa2b742e31948256c1e2685f41e076026c5a1b
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-03-16 09:04:00 -0700

    binary search tree algo update

--- Algorithms/Algos/Spring_Algos/binarySearchTree.js
+++ Algorithms/Algos/Spring_Algos/binarySearchTree.js
@@ -275,6 +275,64 @@
 
   // ************************ END WEDNESDAY ************************
 
+  // ************************ THURSDAY ************************
+    /**
+     * DFS Preorder: (CurrNode, Left, Right)
+     * Converts this BST into an array following Depth First Search preorder.
+     * Example on the fullTree var:
+     * [25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrPreorder(node = this.root, vals = []) {
+        if (node) {
+            vals.push(node.data)
+            this.toArrPreorder(node.left, vals)
+            this.toArrPreorder(node.right, vals)
+        }
+        return vals
+    }
+
+    /**
+     * DFS Inorder: (Left, CurrNode, Right)
+     * Converts this BST into an array following Depth First Search inorder.
+     * See debugger call stack to help understand the recursion.
+     * Example on the fullTree var:
+     * [4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrInorder(node = this.root, vals = []) {
+        if (node) {
+            this.toArrPreorder(node.left, vals)
+            vals.push(node.data)
+            this.toArrPreorder(node.right, vals)
+        }
+        return vals
+    }
+
+    /**
+     * DFS Postorder (Left, Right, CurrNode)
+     * Converts this BST into an array following Depth First Search postorder.
+     * Example on the fullTree var:
+     * [4, 12, 10, 18, 24, 22, 15, 31, 44, 35, 66, 90, 70, 50, 25]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrPostorder(node = this.root, vals = []) {
+        if (node) {
+            this.toArrPreorder(node.left, vals)
+            this.toArrPreorder(node.right, vals)
+            vals.push(node.data)
+        }
+        return vals
+    }
+
+  // ************************ END THURSDAY ************************
+
         // Logs this tree horizontally with the root on the left.
         print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
         if (!node) {
@@ -347,19 +405,22 @@
     /***************** Uncomment after insert method is created. ****************/
     // const fullTree = new BinarySearchTree();
     // fullTree
-    // .insertRecursive(25)
-    // .insertRecursive(15)
-    // .insertRecursive(10)
-    // .insertRecursive(22)
-    // .insertRecursive(4)
-    // .insertRecursive(12)
-    // .insertRecursive(18)
-    // .insertRecursive(24)
-    // .insertRecursive(50)
-    // .insertRecursive(35)
-    // .insertRecursive(70)
-    // .insertRecursive(31)
-    // .insertRecursive(44)
-    // .insertRecursive(66)
-    // .insertRecursive(90);
-    // fullTree.print()
+    // .insert(25)
+    // .insert(15)
+    // .insert(10)
+    // .insert(22)
+    // .insert(4)
+    // .insert(12)
+    // .insert(18)
+    // .insert(24)
+    // .insert(50)
+    // .insert(35)
+    // .insert(70)
+    // .insert(31)
+    // .insert(44)
+    // .insert(66)
+    // .insert(90);
+    // fullTree.print();
+    // console.log(fullTree.toArrPreorder());
+    // console.log(fullTree.toArrInorder());
+    // console.log(fullTree.toArrPostorder());
