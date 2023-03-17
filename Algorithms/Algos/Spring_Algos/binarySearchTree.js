2d3028ba60f1140daaa94dc3d5d8089fef7d5708
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-03-17 08:55:06 -0700

    Binary search tree algo update

--- Algorithms/Algos/Spring_Algos/binarySearchTree.js
+++ Algorithms/Algos/Spring_Algos/binarySearchTree.js
@@ -333,6 +333,100 @@
 
   // ************************ END THURSDAY ************************
 
+  // ************************ FRIDAY ************************
+
+    /**
+     * BFS order: horizontal rows top-down left-to-right.
+     * Converts this BST into an array following Breadth First Search order.
+     * Example on the fullTree var:
+     * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+     * @param {Node} current The current node during the traversal of this tree.
+     * @returns {Array<number>} The data of all nodes in BFS order.
+     */
+    toArrLevelorder(current = this.root, level = []) {
+
+        if (!current) {
+            return level
+        }
+
+        let leftRunner = current.left
+        let rightRunner = current.right
+
+        if (current.data == this.root.data) {
+            level.push(current.data)
+        }
+
+        if (leftRunner) {
+            level.push(leftRunner.data)
+        }
+
+        if (rightRunner) {
+            level.push(rightRunner.data)
+        }
+
+        this.toArrLevelorder(current.left, level)
+        this.toArrLevelorder(current.right, level)
+
+        return level
+    }
+
+    /**
+     * Recursively counts the total number of nodes in this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during the traversal of this tree.
+     * @returns {number} The total number of nodes.
+     */
+    size(node = this.root) {
+        if (!node) {
+            return 0;
+        }
+    
+        return 1 + this.size(node.left) + this.size(node.right);
+    }
+
+    /**
+     * Calculates the height of the tree which is based on how many nodes from
+     * top to bottom (whichever side is taller).
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {number} The height of the tree.
+     */
+    height(node = this.root) {
+        if (!node) {
+            return 0;
+        }
+    
+        return 1 + Math.max(this.height(node.left), this.height(node.right));
+    }
+
+    /**
+     * Determines if this tree is a full tree. A full tree is a tree where every
+     * node has both a left and a right except for the leaf nodes (last nodes)
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {boolean} Indicates if this tree is full.
+     */
+    isFull(node = this.root) {
+        if (!node) {
+            return 0;
+        }
+
+        if (!node.left && !node.right) {
+            return true
+        }
+
+        if (node.left && node.right) {
+            return this.isFull(node.left) && this.isFull(node.right);
+        }
+        return false
+    }
+
+
+  // ************************ END FRIDAY ************************
+
         // Logs this tree horizontally with the root on the left.
         print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
         if (!node) {
@@ -421,6 +515,10 @@
     // .insert(66)
     // .insert(90);
     // fullTree.print();
+    // console.log(fullTree.toArrLevelorder());
+    // console.log(fullTree.size());
+    // console.log(fullTree.height());
+    // console.log(fullTree.isFull());
     // console.log(fullTree.toArrPreorder());
     // console.log(fullTree.toArrInorder());
     // console.log(fullTree.toArrPostorder());
