5024bc208599cdd4100ee5f77ba94cacec8b1659
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-03-30 08:57:38 -0700

    Queues Algo update

--- Algorithms/Algos/Spring_Algos/Queues.js
+++ Algorithms/Algos/Spring_Algos/Queues.js
@@ -60,6 +60,113 @@ class Queue {
     size() {
         return this.items.length;
     }
+
+    /**
+    * Compares this queue to another given queue to see if they are equal.
+    * Avoid indexing the queue items directly via bracket notation, use the
+    * queue methods instead for practice.
+    * Use no extra array or objects.
+    * The queues should be returned to their original order when done.
+    * - Time: O(?).
+    * - Space: O(?).
+    * @param {Queue} q2 The queue to be compared against this queue.
+    * @returns {boolean} Whether all the items of the two queues are equal and
+    *    in the same order.
+    */
+    compareQueues(q2) {
+        if (this.isEmpty() && q2.isEmpty()) return true
+
+        if (this.size() !== q2.size()) return false
+
+        if (this.isEmpty()) return false
+
+        if (q2.isEmpty()) return false
+
+        let count = this.size();
+        let isEqual = true
+
+        while(count > 0) {
+            let queue1 = this.dequeue()
+            let queue2 = q2.dequeue()
+
+            if (queue1 != queue2) isEqual = false
+
+            this.enqueue(queue1)
+            q2.enqueue(queue2)
+            count--;
+        }
+        return isEqual
+    }
+
+    /**
+     * Determines if the queue is a palindrome (same items forward and backwards).
+     * Avoid indexing queue items directly via bracket notation, instead use the
+     * queue methods for practice.
+     * Use only 1 stack as additional storage, no other arrays or objects.
+     * The queue should be returned to its original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isPalindrome() {
+        let isPalin = true;
+        const stack = new Stack(),
+        len = this.size();
+
+        for (let i = 0; i < len; i++) {
+            let dequeued = this.dequeue();
+            stack.push(dequeued);
+            // add it back so the queue items and order is restored at the end
+            this.enqueue(dequeued);
+        }
+
+        for (let i = 0; i < len; i++) {
+            let dequeued = this.dequeue();
+            let popped = stack.pop();
+
+            if (popped !== dequeued) {
+                isPalin = false;
+            }
+
+            // add it back so the queue items and order is restored at the end
+            this.enqueue(dequeued);
+        }
+        return isPalin;
+    }
+
+    /**
+     * Determines whether the sum of the left half of the queue items is equal to
+     * the sum of the right half. Avoid indexing the queue items directly via
+     * bracket notation, use the queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queue should be returned to it's original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean} Whether the sum of the left and right halves is equal.
+     */
+    isSumOfHalvesEqual() {
+        let leftHalf = 0;
+        let rightHalf = 0;
+        let count = this.size();
+        let halfway = Math.floor(this.size() / 2)
+
+        while (count > 0) {
+            let dequeueNum = this.dequeue();
+            console.log("Dequeue: ", dequeueNum);
+
+            if (count > halfway) {
+                leftHalf += dequeueNum;
+            console.log("Left: ", leftHalf);
+            }
+            else {
+                rightHalf += dequeueNum;
+            console.log("Right: ", rightHalf);
+            }
+            this.enqueue(dequeueNum);
+            count--;
+        }
+        return leftHalf === rightHalf;
+    }
 }
 
 /* Rebuild the above class using a linked list instead of an array. */
@@ -117,3 +224,13 @@ class QueueList {
         return this.data.length;
     }
 }
+
+const list = new Queue();
+list.enqueue(1)
+list.enqueue(1)
+list.enqueue(5)
+list.enqueue(3)
+list.enqueue(2)
+list.enqueue(2)
+
+console.log(list.isSumOfHalvesEqual())
